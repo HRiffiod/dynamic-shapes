0b9c42145338d1c8c87cd614c549a9142aff9208
Hugo RIFFIOD <hugo@dekibae.com>
2016-06-16 17:08:22 +0200

    Initial Commit

---
+++ DynamicShapes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicShapes
+//
+//  Created by Hugo RIFFIOD on 16/06/2016.
+//  Copyright © 2016 hugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
